03327ebd16d9f3d8cdb644cd4abb326ecb913d1c
juruo888 <juruo888@outlook.com>
2022-07-08 13:23:33 +0800

    VER 1.2.0-alpha.1
    -- 片尾曲

--- compile.py
+++ compile.py
@@ -126,5 +126,5 @@ os.system("copy SO.ico Releases\\ScratchOff_" + version + "\\SO.ico")
 os.system("rd /s /q python")
 os.system("powershell Compress-Archive .\\Releases\\ScratchOff_" + version + "\\ .\\Releases\\ScratchOff_" + version + ".zip")
 if os.environ.get("GITHUB_ACTION"):
-    os.system("powershell \"VERSION=" + version + "\" >> $env:GITHUB_ENV")
-    os.system("powershell \"FILEPATH=Releases\\ScratchOff_" + version + "\" >> $env:GITHUB_ENV")
+    os.system("pwsh \"VERSION=" + version + "\" >> $env:GITHUB_ENV")
+    os.system("pwsh \"FILEPATH=Releases\\ScratchOff_" + version + "\" >> $env:GITHUB_ENV")
