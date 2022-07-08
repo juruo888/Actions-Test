e08efb3645edb569bfc05b07a50861ffb0722263
juruo888 <juruo888@outlook.com>
2022-07-08 14:21:51 +0800

    VER 1.2.0-alpha.1
    -- 哦啊哦

--- .github/compile/version.py
+++ .github/compile/version.py
@@ -13,6 +13,6 @@ rb = json.loads(os.environ["MSG"])[-1].split("\n\n")
 ver = rb[0][4:]
 log = rb[1]
 os.system("powershell \"VERSION=" + "ScratchOff v" + ver + " (" + version + ") \" | Out-File -FilePath >> $env:GITHUB_ENV -Encoding utf8 -Append")
-os.system("powershell \"VER=" + version + + ") \" | Out-File -FilePath >> $env:GITHUB_ENV -Encoding utf8 -Append")
+os.system("powershell \"VER=" + ver + "\" | Out-File -FilePath >> $env:GITHUB_ENV -Encoding utf8 -Append")
 with open("log.txt", "w") as file:
     file.write(log)
